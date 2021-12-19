93e0da824e4df97b1c276ab62d160b6a91b652a7
Priyanka <prettypri70@gmail.com>
2021-12-19 12:42:43 -0700

    Added link to linkedin profile

--- src/app.js
+++ src/app.js
@@ -70,10 +70,9 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "d1efb3288c5633c9a13cc7534de0139c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
+
   axios.get(apiUrl).then(displayForecast);
 }
 
